726f848995bd64b885d854c34df53c28142d433b
andrejasd <andrej_asd@bigmir.net>
2016-07-10 15:18:47 +0300

    Commit #7

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,8 +9,6 @@ use App\Http\Requests;
 use Illuminate\Support\Facades\DB;
 use App\Models\Lead;
 use App\Models\SphereAttr;
-use App\Models\SphereAttrOptions;
-use App\Models\SphereMask;
 
 class TestController extends Controller
 {
@@ -22,100 +20,6 @@ class TestController extends Controller
             ->select('leads.*', 'customers.phone')
             ->get();
 
-/*
-        foreach ($leads as &$lead){
-
-            $sphere_id = Lead::select('sphere_id')->first()['sphere_id'];
-            $sphere_bitmask = 'sphere_bitmask_'.$sphere_id;
-
-            $lead['radio'] = SphereAttr::
-                where('sphere_attributes._type', '=', 'radio')
-                ->join ('leads', 'leads.sphere_id', '=', 'sphere_attributes.sphere_id')
-                ->join('sphere_attribute_options', function ($join){
-                    $join->on('sphere_attributes.id', '=', 'sphere_attribute_options.sphere_attr_id')
-                        ->where('sphere_attribute_options.ctype', '=', 'agent');
-                })
-                ->join($sphere_bitmask, function ($join){
-                    $mask = 'fb_'.SphereAttr::select('id')->first()['id'].'_'.SphereAttrOptions::select('id')->first()['id'];
-                    $join->on('user_id', '=', 'leads.id')
-                        ->where($mask, '=', '1')
-                        ->where('type', '=', 'lead');
-                })
-                ->select('sphere_attribute_options.value','label')
-                ->first();
-
-            $lead['checkbox'] = SphereAttr::
-                where('sphere_attributes._type', '=', 'checkbox')
-                ->join ('leads', 'leads.sphere_id', '=', 'sphere_attributes.sphere_id')
-                ->join('sphere_attribute_options', function ($join){
-                    $join->on('sphere_attributes.id', '=', 'sphere_attribute_options.sphere_attr_id')
-                        ->where('sphere_attribute_options.ctype', '=', 'agent');
-                })
-                ->join($sphere_bitmask, function ($join){
-                    $mask = 'fb_'.SphereAttr::select('id')->first()['id'].'_'.SphereAttrOptions::select('id')->first()['id'];
-                    //echo '<pre>'; print_r($mask); die;
-                    $join->on('user_id', '=', 'leads.id')
-                        ->where($mask, '=', '1')
-                        ->where('type', '=', 'lead');
-                })
-                ->select('sphere_attribute_options.value', 'label')
-                ->first();
-        }
-*/
-
-        $id = 7;
-/*
-        $sphere_id = Lead::
-             select('sphere_id')
-            ->where('id', '=', $id)
-            ->first()['sphere_id'];
-        $sphere_bitmask = 'sphere_bitmask_'.$sphere_id;
-*/
-        $checkbox = SphereAttr::
-        where('sphere_attributes._type', '=', 'checkbox')
-            ->join ('leads', 'leads.sphere_id', '=', 'sphere_attributes.sphere_id')
-            ->where('leads.id', '=', $id)
-            ->join('sphere_attribute_options', function ($join){
-                $join->on('sphere_attributes.id', '=', 'sphere_attribute_options.sphere_attr_id')
-                    ->where('sphere_attribute_options.ctype', '=', 'agent');
-            })
-            ->select('sphere_attributes.id as attr_id', 'sphere_attribute_options.id as attr_opt_id', 'sphere_attribute_options.value',
-                    'sphere_attributes.label', 'leads.id as leads_id', 'leads.sphere_id')
-            ->first();
-
-        $sphere_bitmask = 'sphere_bitmask_'.$checkbox['sphere_id'];
-        $AID = $checkbox['attr_id'];
-        $OID = $checkbox['attr_opt_id'];
-        $mask = 'fb_'.$AID.'_'.$OID;
-
-/*
-        $fb_aid_oid = new SphereMask($checkbox['sphere_id']);
-        $fb_aid_oid
-            ->select($mask)
-            ->where('user_id', '=', $checkbox['leads_id'])
-            ->where('type', '=', 'lead')
-            ->first()
-            ;
-*/
-        $a = DB::table($sphere_bitmask)
-                ->select($mask)
-                ->where('user_id', '=', $checkbox['leads_id'])
-                ->where('type', '=', 'lead')
-                ->first()
-            ;
-
-        echo '<pre>'; var_dump($a->$mask); die;
-        /*
-         *             ->join($sphere_bitmask, function ($join){
-
-                //echo '<pre>'; print_r($mask); die;
-                $join->on('user_id', '=', 'leads.id')
-                    ->where($mask, '=', '1')
-                    ->where('type', '=', 'lead');
-            })
-         */
-
-
         return view('test_page', ['leads' => $leads]);
     }
 
@@ -124,11 +28,11 @@ class TestController extends Controller
         if ( $_GET ) {
             $id = $_GET['id'];
 
-            $sphere_id = Lead::
-                select('sphere_id')
-                ->where('id', '=', $id)
-                ->first()['sphere_id'];
-            $sphere_bitmask = 'sphere_bitmask_'.$sphere_id;
+            $leads = Lead::
+                where ('leads.id', '=', $id)
+                ->join('customers', 'leads.customer_id', '=', 'customers.id')
+                ->select('leads.date', 'leads.name', 'leads.email', 'customers.phone')
+                ->first();
 
             $radio = SphereAttr::
                 where('sphere_attributes._type', '=', 'radio')
@@ -138,43 +42,57 @@ class TestController extends Controller
                     $join->on('sphere_attributes.id', '=', 'sphere_attribute_options.sphere_attr_id')
                         ->where('sphere_attribute_options.ctype', '=', 'agent');
                 })
-                ->join($sphere_bitmask, function ($join){
-                    $AID = SphereAttr::select('id')->first()['id'];
-                    $OID = SphereAttrOptions::select('id')->first()['id'];
-                    $mask = 'fb_'.$AID.'_'.$OID;
-                    $join->on('user_id', '=', 'leads.id')
-                        ->where($mask, '=', '1')
-                        ->where('type', '=', 'lead');
-                })
-                ->select('sphere_attribute_options.value','label')
+                ->select('sphere_attributes.id as attr_id', 'sphere_attribute_options.id as attr_opt_id', 'sphere_attribute_options.value',
+                    'sphere_attributes.label', 'leads.id as leads_id', 'leads.sphere_id')
                 ->first();
 
             $checkbox = SphereAttr::
-                where('sphere_attributes._type', '=', 'checkbox')
+              where('sphere_attributes._type', '=', 'checkbox')
                 ->join ('leads', 'leads.sphere_id', '=', 'sphere_attributes.sphere_id')
                 ->where('leads.id', '=', $id)
                 ->join('sphere_attribute_options', function ($join){
                     $join->on('sphere_attributes.id', '=', 'sphere_attribute_options.sphere_attr_id')
                         ->where('sphere_attribute_options.ctype', '=', 'agent');
                 })
-                ->join($sphere_bitmask, function ($join){
-                    $mask = 'fb_'.SphereAttr::select('id')->first()['id'].'_'.SphereAttrOptions::select('id')->first()['id'];
-                    //echo '<pre>'; print_r($mask); die;
-                    $join->on('user_id', '=', 'leads.id')
-                        ->where($mask, '=', '1')
-                        ->where('type', '=', 'lead');
-                })
-                ->select('sphere_attribute_options.value', 'label')
+                ->select('sphere_attributes.id as attr_id', 'sphere_attribute_options.id as attr_opt_id', 'sphere_attribute_options.value',
+                    'sphere_attributes.label', 'leads.id as leads_id', 'leads.sphere_id')
                 ->first();
-
 
             $data = array(
                 'id' => $id,
+                'date' => $leads->date,
+                'name' => $leads->name,
+                'email' => $leads->email,
+                'phone' => $leads->phone,
                 'radio_label' => $radio->label,
-                'radio_value' => $radio->value,
+                'radio_value' => '',
                 'checkbox_label' => $checkbox->label,
-                'checkbox_value' => $checkbox->value,
+                'checkbox_value' => ''
             );
+
+            $sphere_bitmask = 'sphere_bitmask_'.$radio['sphere_id'];
+            $AID = $radio['attr_id'];
+            $OID = $radio['attr_opt_id'];
+            $mask = 'fb_'.$AID.'_'.$OID;
+            $fb_aid_oid = DB::table($sphere_bitmask)
+                ->select($mask)
+                ->where('user_id', '=', $radio['leads_id'])
+                ->where('type', '=', 'lead')
+                ->first()
+            ;
+            $data['radio_value'] = ($fb_aid_oid->$mask === 1) ? $radio->value : '';
+
+            $sphere_bitmask = 'sphere_bitmask_'.$checkbox['sphere_id'];
+            $AID = $checkbox['attr_id'];
+            $OID = $checkbox['attr_opt_id'];
+            $mask = 'fb_'.$AID.'_'.$OID;
+            $fb_aid_oid = DB::table($sphere_bitmask)
+                ->select($mask)
+                ->where('user_id', '=', $checkbox['leads_id'])
+                ->where('type', '=', 'lead')
+                ->first()
+            ;
+            $data['checkbox_value'] = ($fb_aid_oid->$mask === 1) ? $checkbox->value : '';
 
             echo json_encode($data);
         }
--- resources/views/test_page.blade.php
+++ resources/views/test_page.blade.php
@@ -9,28 +9,72 @@
     function detail(id) {
       var data = {'id' : id};
       console.log(data);
-/*
-      $.ajax({
-        type: "get",
-        url: 'detail',
-        data: "",
-        success: function() {
-          console.log("Geodata sent");
-        }
-      });
-*/
 
       $.get('detail', data, function (data) {
         data = JSON.parse(data);
         console.log(data);
+
+        $("#date").text(data['date']);
+        $("#name").text(data['name']);
+        $("#phone").text(data['phone']);
+        $("#email").text(data['email']);
+        $("#radio_label").text(data['radio_label']);
+        $("#radio_value").text(data['radio_value']);
+        $("#checkbox_label").text(data['checkbox_label']);
+        $("#checkbox_value").text(data['checkbox_value']);
+
+        $("#ModalBox").modal('show');
       });
 
       return false;
     }
   </script>
 
-
-
+  <!-- модальное окно -->
+  <div id="ModalBox" class="modal fade">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+          <h4 class="modal-title">Детальная информация</h4>
+        </div>
+        <div class="modal-body">
+          <table class="table table-bordered">
+              <tr>
+                <th>icon</th>
+                <td id="icon"></td>
+              </tr>
+              <tr>
+                <th>date</th>
+                <td id="date"></td>
+              </tr>
+              <tr>
+                <th>name</th>
+                <td id="name"></td>
+              </tr>
+              <tr>
+                <th>phone</th>
+                <td id="phone"></td>
+              </tr>
+              <tr>
+                <th>email</th>
+                <td id="email"></td>
+              </tr>
+              <tr>
+                <th id="radio_label"></th>
+                <td id="radio_value"></td>
+              </tr><tr>
+                <th id="checkbox_label"></th>
+                <td id="checkbox_value"></td>
+              </tr>
+          </table>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <table class="table table-bordered table-hover">
     <thead>
